e4a9a6b8c17a3346124dbb8b36d9ec802423e370
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-04-17 11:46:04 -0400

    Removed unneeded routes

--- StockScraper/Controllers/CurrentStockInfoesController.cs
+++ StockScraper/Controllers/CurrentStockInfoesController.cs
@@ -72,63 +72,6 @@ namespace StockScraper.Controllers
             return View(currentStockInfo);
         }
 
-        // GET: CurrentStockInfoes/Edit/5
-        public ActionResult Edit(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            CurrentStockInfo currentStockInfo = db.CurrentStockInfoes.Find(id);
-            if (currentStockInfo == null)
-            {
-                return HttpNotFound();
-            }
-            return View(currentStockInfo);
-        }
-
-        // POST: CurrentStockInfoes/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Stock_Id,Symbol,PercentChange,AverageVolume,LastPrice,MarketTime,OpenPrice,HighPrice,LowPrice,YearWeekHigh,YearWeekLow,Date")] CurrentStockInfo currentStockInfo)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(currentStockInfo).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(currentStockInfo);
-        }
-
-        // GET: CurrentStockInfoes/Delete/5
-        public ActionResult Delete(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            CurrentStockInfo currentStockInfo = db.CurrentStockInfoes.Find(id);
-            if (currentStockInfo == null)
-            {
-                return HttpNotFound();
-            }
-            return View(currentStockInfo);
-        }
-
-        // POST: CurrentStockInfoes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
-        {
-            CurrentStockInfo currentStockInfo = db.CurrentStockInfoes.Find(id);
-            db.CurrentStockInfoes.Remove(currentStockInfo);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
         protected override void Dispose(bool disposing)
         {
             if (disposing)
--- StockScraper/Controllers/HistoryOfStockInfoesController.cs
+++ StockScraper/Controllers/HistoryOfStockInfoesController.cs
@@ -22,13 +22,13 @@ namespace StockScraper.Controllers
         }
 
         // GET: HistoryOfStockInfoes/Details/5
-        public ActionResult Details(byte? id)
+        public ActionResult Details(byte? id, DateTime date)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            HistoryOfStockInfo historyOfStockInfo = db.HistoryOfStockInfoes.Find(id);
+            HistoryOfStockInfo historyOfStockInfo = db.HistoryOfStockInfoes.Find(id, date);
             if (historyOfStockInfo == null)
             {
                 return HttpNotFound();
@@ -61,64 +61,6 @@ namespace StockScraper.Controllers
             return View(historyOfStockInfo);
         }
 
-        // GET: HistoryOfStockInfoes/Edit/5
-        public ActionResult Edit(byte? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            HistoryOfStockInfo historyOfStockInfo = db.HistoryOfStockInfoes.Find(id);
-            if (historyOfStockInfo == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.Symbol = new SelectList(db.CurrentStockInfoes, "Symbol", "PercentChange", historyOfStockInfo.Symbol);
-            return View(historyOfStockInfo);
-        }
-
-        // POST: HistoryOfStockInfoes/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "StockId,Symbol,PercentChange,AverageVolume,LastPrice,MarketTime,OpenPrice,HighPrice,LowPrice,YearWeekHigh,YearWeekLow,Date")] HistoryOfStockInfo historyOfStockInfo)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(historyOfStockInfo).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.Symbol = new SelectList(db.CurrentStockInfoes, "Symbol", "PercentChange", historyOfStockInfo.Symbol);
-            return View(historyOfStockInfo);
-        }
-
-        // GET: HistoryOfStockInfoes/Delete/5
-        public ActionResult Delete(byte? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            HistoryOfStockInfo historyOfStockInfo = db.HistoryOfStockInfoes.Find(id);
-            if (historyOfStockInfo == null)
-            {
-                return HttpNotFound();
-            }
-            return View(historyOfStockInfo);
-        }
-
-        // POST: HistoryOfStockInfoes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(byte id)
-        {
-            HistoryOfStockInfo historyOfStockInfo = db.HistoryOfStockInfoes.Find(id);
-            db.HistoryOfStockInfoes.Remove(historyOfStockInfo);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
 
         protected override void Dispose(bool disposing)
         {
